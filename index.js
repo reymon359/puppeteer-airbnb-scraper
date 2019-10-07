c7fa0b10a92d4a7f566c0200048667772fb3ae95
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-10-07 06:53:56 +0200

    Opening Homes in a separate page

--- index.js
+++ index.js
@@ -9,20 +9,18 @@ const sample = {
     cost: 350
 
 };
-
+let browser;
 const URL = 'https://www.airbnb.com/s/Copenhagen/homes?refinement_paths%5B%5D=%2Fhomes&click_referer=t%3ASEE_ALL%7Csid%3A9ea0a18e-f8e0-4eec-8840-b5a4290dfd22%7Cst%3ASTOREFRONT_DESTINATION_GROUPINGS&title_type=HOMES_WITH_LOCATION&query=Copenhagen&allow_override%5B%5D=&s_tag=UrkEXloL&section_offset=7&items_offset=36';
 
 async function scrapeHomesInIndexPage(url) {
     try {
-
-        const browser = await puppeteer.launch({ headless: false });
         const page = await browser.newPage();
         await page.goto(url);
         const html = await page.evaluate(() => document.body.innerHTML);
         const $ = await cheerio.load(html);
 
         const homes = $("[itemprop='url']")
-            .map((i, element) => 'https://' + $(element).attr('content'))
+            .map((i, element) => 'https://airbnb.com' + $(element).attr('content').replace('null', ''))
             .get();
         return homes;
 
@@ -31,9 +29,24 @@ async function scrapeHomesInIndexPage(url) {
     }
 }
 
+
+async function scrapeDescriptionPage(url, page) {
+    try {
+        await page.goto(url);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 async function main() {
+
+    browser = await puppeteer.launch({ headless: false });
+    const descriptionPage = await browser.newPage();
     const homes = await scrapeHomesInIndexPage(URL);
     console.log(homes);
+    for (let i = 0; i < homes.length; i++) {
+        await scrapeDescriptionPage(homes[i], descriptionPage);
+    }
 }
 
 main();
